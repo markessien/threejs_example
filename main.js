d2ac9a0f885a4c0ebe09602ffa5eebe6e3aa4831
Mark Essien <markessien@gmail.com>
2023-10-01 12:06:10 +0200

    added the threejs animation to a div

--- main.js
+++ main.js
@@ -2,63 +2,72 @@ import * as THREE from 'three';
 import { FBXLoader } from 'three/examples/jsm/loaders/FBXLoader'
 import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls'
 
-// Create Scene
-const scene = new THREE.Scene()
-scene.add(new THREE.AxesHelper(5))
+window.addEventListener('load', function () {
+    // Create Scene
+    const scene = new THREE.Scene()
+    scene.add(new THREE.AxesHelper(5))
 
-// Add a light
-const light = new THREE.PointLight(0xffffff, 1000)
-light.position.set(2.5, 7.5, 15)
-scene.add(light)
+    // Add a light
+    const light = new THREE.PointLight(0xffffff, 1000)
+    light.position.set(2.5, 7.5, 15)
+    scene.add(light)
 
-// Add a camera
-const camera = new THREE.PerspectiveCamera( 75, window.innerWidth / window.innerHeight, 0.1, 1000 );
-camera.position.set(0.8, 1.4, 1.0)
+    // Add a camera
+    const camera = new THREE.PerspectiveCamera( 75, window.innerWidth / window.innerHeight, 0.1, 1000 );
+    camera.position.set(0.8, 1.4, 1.0)
 
-// Add renderer and put in the DOM
-const renderer = new THREE.WebGLRenderer();
-renderer.setSize( window.innerWidth, window.innerHeight );
-document.body.appendChild( renderer.domElement );
+    // Add renderer and put in the DOM
+    const renderer = new THREE.WebGLRenderer();
+    renderer.setPixelRatio( window.devicePixelRatio );
+    renderer.setSize( window.innerWidth, window.innerHeight );
+    renderer.setSize( 200, 200 );
 
-// Add the orbit controls
-const controls = new OrbitControls(camera, renderer.domElement)
-controls.enableDamping = true
-controls.target.set(0, 1, 0)
+    var container = document.getElementById( 'canvas' );
+    container.appendChild( renderer.domElement );
 
-var mixer
-var modelReady = false
+    // Add the canvas to the DOM
+    // document.body.appendChild( renderer.domElement );
 
-// Load our FBX model from the directory
-var loader = new FBXLoader();
-loader.load("Walking.fbx", function(object) {
+    // Add the orbit controls
+    const controls = new OrbitControls(camera, renderer.domElement)
+    controls.enableDamping = true
+    controls.target.set(0, 1, 0)
 
-    // Scale and position the model
-    object.scale.set(0.01, 0.01, 0.01)
-    object.position.set(0, 0, 0)
+    var mixer
+    var modelReady = false
 
-    // Start the default animation
-    mixer = new THREE.AnimationMixer(object);
-    var action = mixer.clipAction(object.animations[0]);
-    action.play();
+    // Load our FBX model from the directory
+    var loader = new FBXLoader();
+    loader.load("Walking.fbx", function(object) {
 
-    // Add it to the scene
-    scene.add(object);
+        // Scale and position the model
+        object.scale.set(0.007, 0.007, 0.007)
+        object.position.set(0, 0, 0)
 
-    modelReady = true
+        // Start the default animation
+        mixer = new THREE.AnimationMixer(object);
+        var action = mixer.clipAction(object.animations[0]);
+        action.play();
 
+        // Add it to the scene
+        scene.add(object);
+
+        modelReady = true
+
+    });
+
+
+    // Add animation routine
+    var clock = new THREE.Clock();
+    function animate() {
+        requestAnimationFrame( animate );
+
+        // Call the animate on the objec
+        if (modelReady) mixer.update(clock.getDelta());
+
+        renderer.render( scene, camera );
+    }
+
+
+    animate();
 });
-
-
-// Add animation routine
-var clock = new THREE.Clock();
-function animate() {
-    requestAnimationFrame( animate );
-
-    // Call the animate on the objec
-    if (modelReady) mixer.update(clock.getDelta());
-
-	renderer.render( scene, camera );
-}
-
-
-animate();
